77cd6fb03e93c097181b4a48fff7df63480045a8
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-18 16:06:55 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            persona x = new persona("giuseppe", "Garibaldi",26);
+            Console.WriteLine(x.getNome());
+            Console.WriteLine(x.getCognome());
+            Console.WriteLine(x.getEta());
+            Console.WriteLine(x.GetDettagli());
+
+        }
+    }
+}
---
+++ ConsoleApp1/persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+
+        public persona(string nome , string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+        public string getNome() {  return Nome; }
+        public string getCognome() { return Cognome; }
+        public int getEta() { return Eta; }
+        public string GetDettagli()
+        {
+            return "Nome: " + Nome + ", Cognome: " + Cognome + ", Età: " + Eta;
+        }
+    }
+}
